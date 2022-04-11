4d8de2ca2731b49feb4c7d74d5a56ddaad8ee256
lukeeboy-coding <luke.crawford12356@gmail.com>
2022-04-11 18:51:30 +0000

    lol

---
+++ idek.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <lukelib.h>
+
+
+
+int database() {
+    
+}
---
+++ lukelib.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+struct login {
+    int email;
+    float password;
+    
+}
